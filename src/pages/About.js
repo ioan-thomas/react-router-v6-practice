4335dba09c55b5eb012f6d619511214d1ccdb169
Ioan Thomas <ioanthomas21@gmail.com>
2022-06-19 13:19:23 +0100

    implemented nested routing in v6

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,4 @@
-import { Route, useNavigate } from "react-router-dom"
+import { Route, Routes, useNavigate } from "react-router-dom"
 
 // nested routes
 import Offers from "./Offers"
@@ -13,10 +13,9 @@ export default function About() {
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime tempora nisi eligendi quod ut illo cumque quibusdam reprehenderit veniam. Cum eaque, nostrum fugit ut inventore assumenda perspiciatis sed maiores numquam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati nam repellat hic dolore officia sequi quis quod commodi iusto doloremque. Corporis in et placeat unde sapiente perspiciatis minus! Recusandae, ad!</p>
 
       <button onClick={() => navigate('/products')}>See our products</button>
-
-      {/* <Route path="/about/offers">
-        <Offers />
-      </Route> */}
+      <Routes>
+        <Route path="offers"  element={<Offers />}/>
+      </Routes>
     </div>
   )
 }
